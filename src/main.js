d0ad888f16ece956618c8236e1bdf788ef040dcb
Rob <rverplanke@gmail.com>
2023-11-25 21:12:17 +0100

    reorganize code

--- src/main.js
+++ src/main.js
@@ -4,24 +4,16 @@ import Board from './modules/board.js';
 const board = new Board();
 const graph = new Map();
 
-// Create nodes with its coordinates as key value
+// Create nodes with its coordinates as key and posibble moves as value
 function populateSquares() {
   for (let i = 0; i < board.size; i++) {
     for (let j = 0; j < board.size; j++) {
-      graph.set([i, j], null);
+      graph.set([i, j], board.getValidMoves([i, j]));
     }
   }
 }
 
-// Set valid posibble moves for each node
-function setEdges() {
-  graph.forEach((value, key) => graph.set(key, board.getValidMoves(key)));
-}
-
 // Set nodes
 populateSquares();
-
-// Connect nodes
-setEdges();
 
 console.log(graph);
